a40d6094c7603e95d6960ee26a8525c6df216222
matthewsutanto <41150197+matthewsutanto@users.noreply.github.com>
2021-11-02 11:58:32 +0700

    update deploy

---
+++ todolist_ReactiveForm/server.js
@@ -0,0 +1,29 @@
+function requiredHTTPS(req, res, next) {
+    //membuat semua request yang sebelumnya http biasa menjadi https
+    if (
+        !res.secure
+        // untuk server yang di deploy di heroku
+        && res.get('x-forwarded-proto') !== "https"
+    ) {
+        return res.redirect(
+            'https://' + req.get('host') + req.url
+        )
+    }
+    next();
+}
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080
+
+// app.use(requiredHTTPS);
+// nama app di package.json
+app.use(express.static('./dist/todolist'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/todolist/' }),
+);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
